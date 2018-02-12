52d808c437b80fa9caf7d015d3c82deb2cc8fee6
Hal Perry Cardenas <halperry@Hals-MacBook-Pro.local>
2018-02-12 16:42:51 -0600

    Added all links to navigate through page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,15 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
+    
+  end
+
+  def edit
+    @article = Article.find(params[:id])
     
   end
 
@@ -17,6 +26,17 @@ class ArticlesController < ApplicationController
 
   def show 
     @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      flash[:notice] = "Article was succesfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   private
