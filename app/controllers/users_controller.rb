a3a32ca662681e2c4c56ceaf36dd9c0248b46cd8
Hal Perry Cardenas <halperry@Hals-MacBook-Pro.local>
2018-02-21 11:03:51 -0600

    Creat show action with gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,18 +18,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def show
+    @user = User.find(params[:id])
+    
+  end
+
   def update
     @user = User.find(params[:id])
 
     if @user.update(user_params)
       flash[:success] = "Your account was edited succesfully"
       redirect_to articles_path
-
     else
       render 'edit'
     end
-
-    
   end
 
   private 
